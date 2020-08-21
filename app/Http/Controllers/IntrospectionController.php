d7598e9c7522d96e567b6a4d6d0f75e7d43de305
Nick Lobaton <nickm3gtr@gmail.com>
2020-08-21 12:05:09 +0800

    added introspection endpoint

---
+++ app/Http/Controllers/IntrospectionController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class IntrospectionController extends Controller
+{
+    public function index(Request $request)
+    {
+        $token = $request->input('token');
+        $client_id = $request->input('client_id');
+        $client_secret = $request->input('client_secret');
+
+        // bool variable to determine if token is active
+        $active = false;
+
+        // Decrypt access token
+        $tokenParts = explode(".", $token);
+        $tokenHeader = json_decode(base64_decode($tokenParts[0]), true);
+        $tokenPayload = json_decode(base64_decode($tokenParts[1]), true);
+
+        // Check if token header is invalid
+        if (is_null($tokenHeader)) {
+            return response()->json([
+                'error' => 'invalid_request',
+            ], 400);
+        }
+
+        // Check token payload jti in DB if present
+        $access_token_value = DB::table('oauth_access_tokens')->find($tokenPayload['jti']);
+
+        if(count((array)$access_token_value) == 0) {
+            return response()->json([
+                'error' => 'invalid_request',
+            ], 400);
+        }
+
+        // if token is not revoked make $active true
+        if($access_token_value->revoked == 0) {
+            $active = true;
+        }
+
+        // convert $access_token_value->scopes array into string and remove unnecessary characters
+        $scope = implode(",",(array)$access_token_value->scopes);
+        $scope = str_replace('[', "", $scope);
+        $scope = str_replace(']', "", $scope);
+        $scope = str_replace('"', "", $scope);
+        $scope = stripslashes($scope);
+
+//        print($scope);
+
+//        print_r($access_token_value);
+
+        return response()->json([
+            'active' => $active,
+            'client_id' => $access_token_value->client_id,
+            'user_id' => $access_token_value->user_id,
+            'scope' => $scope,
+        ], 200);
+    }
+}
